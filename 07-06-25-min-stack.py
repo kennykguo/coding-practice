b329d31ed7ec8356591bad7858941f8fade6fab6
kennykguo <kennykguo@gmail.com>
2025-07-06 23:33:00 -0400

    changes

--- 07-06-25-min-stack.py
+++ 07-06-25-min-stack.py
@@ -20,3 +20,36 @@ class MinStack:
                 res = self.stack[i]
         
         return res
+
+
+
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+        self.min_val = float("infinity")
+         
+    def push(self, val: int) -> None:
+        # if less than current min val, push it onto
+        if val < self.min_val:
+            self.min_val = val
+            self.min_stack.append(self.min_val) # push new lowest value to min stack
+        else:
+            self.min_stack.append(self.min_stack[-1]) # push most recent value to min stack
+        self.stack.append(val) # add to actual stack
+        return
+
+    def pop(self) -> None:
+        val = self.min_stack.pop() # pop from min stack and get val
+        if len(self.min_stack) == 0: # check if stack is empty
+            self.min_val = float("infinity")
+        else:
+            self.min_val = self.min_stack[-1] # if not empty, then replace min_val with newest min_val
+        return self.stack.pop() # pop from actual stack
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.min_val
